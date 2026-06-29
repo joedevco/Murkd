6e9c84203bcde0e808022224c304e81944299e37
Joseph <testinge015@gmail.com>
2026-06-28 21:35:26 -0700

    add app.config.js for build variant package names

---
+++ app.config.js
@@ -0,0 +1,29 @@
+const APP_VARIANT = process.env.EAS_BUILD_PROFILE ?? 'production';
+
+const variants = {
+  development: {
+    name: 'Murkd Dev',
+    android: { package: 'com.genigeek.Murkd.dev' },
+    ios: { bundleIdentifier: 'com.genigeek.Murkd.dev' },
+  },
+  preview: {
+    name: 'Murkd Preview',
+    android: { package: 'com.genigeek.Murkd.preview' },
+    ios: { bundleIdentifier: 'com.genigeek.Murkd.preview' },
+  },
+  production: {
+    name: 'Murkd',
+    android: { package: 'com.genigeek.Murkd' },
+    ios: { bundleIdentifier: 'com.genigeek.Murkd' },
+  },
+};
+
+const config = require('./app.json');
+
+config.expo.name = variants[APP_VARIANT].name;
+config.expo.android.package = variants[APP_VARIANT].android.package;
+if (config.expo.ios) {
+  config.expo.ios.bundleIdentifier = variants[APP_VARIANT].ios.bundleIdentifier;
+}
+
+module.exports = config;
